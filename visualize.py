3c1eb7e3c2a71b2e31f47880a89bf415bb02aade
WSL-Hyeonbeen <lhbsharp@khu.ac.kr>
2023-05-30 20:26:08 +0900

    compute_iou with updated dataset

--- visualize.py
+++ visualize.py
@@ -73,45 +73,42 @@ def plot_predictions():
                 plot_mask(img, mask_label)
 
 
-def compute_miou():
+def compute_miou(sam):
     from utils.sam_loss import SamLoss
     metric = SamLoss()
-    total_annotations = len(glob.glob(f'{targets_path}/*.png'))-len(glob.glob(f'{targets_path}/*.jpg'))
+    total_annotations_ = glob.glob(f'{targets_path}/*.png')
+    total_annotations=total_annotations_.copy()
+    for anno in total_annotations_:
+        if len(anno.split('-'))<3:
+            total_annotations.remove(anno)
+        elif len(anno.split('-'))==3:
+            for key in ['hair', 'ear', 'eye', 'ebrow', 'mouth', 'nose']:
+                if key in anno:
+                    total_annotations.remove(anno)
+                    break
     print(f"Computing IoU scores on {targets_path}")
 
     scores = []
     scores_tuned = []
     count=0
-    for i in glob.glob(f'{targets_path}/*.jpg'):
-        img = loadimg(i)
-        name_img = '.'.join(os.path.basename(i).split('.')[:-1])
-        for m in glob.glob(f'{targets_path}/{name_img}*.png'):
-            if len(os.path.basename(m).split('-')) >= 3:
-                # load mask
-                mask_label = loadmask(m)
-                # forward
-                with torch.no_grad():
-                    mask, _, __ = SamForward(
-                        sam, img, mask_label, multimask_output=False)
-                    # mask_tuned, _, __ = SamForward(
-                    #     sam_tuned, img, mask_label, multimask_output=False)
-                # logits
-                mask_label=mask_label.type(torch.bool)
-                mask=(mask > sam.mask_threshold).cpu()
-                # mask_tuned=(mask_tuned > sam_tuned.mask_threshold).cpu()
-                # evaluate
-                score = metric.iou_logits(mask, mask_label)
-                # score_tuned = metric.iou_logits(mask_tuned, mask_label)
-                scores.append(scoretm)
-                # scores_tuned.append(score_tuned)
-                count+=1
-                print(
-                    f"{count}/{total_annotations}: {score.item():.6f}")
-                # print(
-                    # f"{count}/{total_annotations}: {score.item():.6f}, {score_tuned.item():.6f}")
+    for m in total_annotations:
+        img = loadimg(m.split('-')[0]+'.jpg')
+        mask_label = loadmask(m)
+        # forward
+        with torch.no_grad():
+            mask, _, __ = SamForward(
+                sam, img, mask_label, multimask_output=False)
+        # logits
+        mask_label=mask_label.type(torch.bool)
+        mask=(mask > sam.mask_threshold).cpu()
+        # evaluate
+        score = metric.iou_logits(mask, mask_label)
+        scores.append(score)
+        count+=1
+        print(
+            f"{count}/{len(total_annotations)}: {score.item():.6f}")
     print()
-    print(f"Original SAM: {torch.cat(scores).mean()}")
-    # print(f"Fine-tuned SAM: {torch.cat(scores_tuned).mean()}")
+    print(f"SAM: {torch.cat(scores).mean()}")
 
 
 if __name__ == '__main__':
@@ -119,23 +116,27 @@ if __name__ == '__main__':
     checkpoint = 'model/sam_vit_h_4b8939.pth'
     device = 'cuda'
     sam = sam_model_registry['vit_h'](
-        checkpoint=checkpoint).to(device)  # ViT-Huge
+        checkpoint=checkpoint)  # ViT-Huge
 
     # load fine-tuned decoder
-    model_path = 'model/finetuned_decoder_epoch03_batch0169_score0.3561.pt'
+    model_path = 'model/finetuned_decoder_epoch03_batch0166_score0.3910.pt'
     sam_tuned = deepcopy(sam)
     sam_tuned.mask_decoder.load_state_dict(torch.load(model_path))
     sam_tuned_log = torch.load(model_path+'log')
 
+    
+    sam.to(device)
+    # sam_tuned.to(device)
     sam.eval()
     sam_tuned.eval()
 
     # validation data path
-    targets_path = 'images/val'
+    targets_path = 'images/train'
 
     # plot
-    plot_template()
-    plot_log()
+    # plot_template()
+    # plot_log()
     # quit()
-    plot_predictions()
-    # compute_miou()
+    # plot_predictions()
+    compute_miou(sam)
+    # compute_miou(sam_tuned)
